2cba9909090bbd04480606185b24c7cf4b5c2d42
Lukas Schneglberger <l.schneglberger@gmail.com>
2022-09-22 11:03:28 +0200

    Coded Interface, Added class: 'EratosthenesPrimeSieve'

--- PrimeSieve/src/com/company/PrimeSieve.java
+++ PrimeSieve/src/com/company/PrimeSieve.java
@@ -1,5 +1,9 @@
 package com.company;
 
 public interface PrimeSieve {
-    
+// Ueberprueft ob p eine Primza hl ist oder nicht.
+    public boolean isPrime (int p);
+    // Gibt alle Primzahlen bis zur Obergrenze auf der Konsole aus.
+    public void printPrimes();
 }
+
