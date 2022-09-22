4a95793cfbdf94298c80ea491aed6723f63b1f29
Lukas Schneglberger <l.schneglberger@gmail.com>
2022-09-22 11:22:22 +0200

    Added logic to fill ArrayList

--- PrimeSieve/src/com/company/EratosthenesPrimeSieve.java
+++ PrimeSieve/src/com/company/EratosthenesPrimeSieve.java
@@ -1,4 +1,27 @@
 package com.company;
 
-public class EratosthenesPrimeSieve {
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class EratosthenesPrimeSieve implements  PrimeSieve{
+
+
+    @Override
+    public boolean isPrime(int p) {
+        List<Integer> numbers = Stream.iterate(1, n -> n + 1)
+                .limit(p)
+                .collect(Collectors.toList());
+
+        for(int n : numbers){
+
+        }
+    }
+
+    @Override
+    public void printPrimes() {
+        
+    }
+
 }
